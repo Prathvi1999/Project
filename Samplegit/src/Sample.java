dbc5caabc3d57544dea59c20e5ffc0d52d09612d
hp <hp@LAPTOP-76NHQ9AJ>
2021-02-25 16:44:43 +0530

    Updated commit

--- Samplegit/src/Sample.java
+++ Samplegit/src/Sample.java
@@ -4,6 +4,7 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Sample class");
 		System.out.println("Hi");
+		System.out.println("Updates messaga: Good Evening");
 
 	}
 
